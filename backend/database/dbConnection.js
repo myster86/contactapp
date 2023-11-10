4b141d21bffd82168ccc17e11a502c160bc09665
Ghelouce Mehdi <gheloucemehdi@gmail.com>
2023-11-10 11:12:37 +0100

    Creating a MERN stack project

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.CONNECTION_URI);
+    console.log(
+      "Database connected successfully :",
+      connection.connection.host,
+      connection.connection.name
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = connectDB;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Contacts from "./Components/Contacts";
+
+function App() {
+  return (
+    <>
+      <Contacts />
+    </>
+  );
+}
+
+export default App;
